c9f6a44f7713ddbbde207560e0d141698c146029
Arun Malik <3743409+malikarun@users.noreply.github.com>
2021-12-17 21:11:16 +1000

    Fix zsh setup identation

--- config/zsh/install.zsh
+++ config/zsh/install.zsh
@@ -25,25 +25,25 @@ fi
 # Install OhMyZsh
 ZSH_PATH="$HOME/.oh-my-zsh"
 if [ ! -d $ZSH_PATH ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 HIGHLIGHT_PATH="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 #  Syntax highlighting
 if [ ! -d $HIGHLIGHT_PATH ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HIGHLIGHT_PATH
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HIGHLIGHT_PATH
 fi
 
 BULLET_TRAIN_THEME_PATH="${ZSH_THEME:-$HOME/.oh-my-zsh/themes}/bullet-train.zsh-theme"
-#  Syntax highlighting
+#  Theme
 if [ ! -d $BULLET_TRAIN_THEME_PATH ]; then
-    curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o $BULLET_TRAIN_THEME_PATH
+  curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o $BULLET_TRAIN_THEME_PATH
 fi
 
 AUTO_SUGGESTION_PATH="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 # Autosuggestions
 if [ ! -d $AUTO_SUGGESTION_PATH ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions $AUTO_SUGGESTION_PATH
+  git clone https://github.com/zsh-users/zsh-autosuggestions $AUTO_SUGGESTION_PATH
 fi
 
 # Powerline font
