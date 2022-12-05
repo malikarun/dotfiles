4ae1c4792cc45cc7416a0db4c0c4fbfd326312cf
Arun Malik <3743409+malikarun@users.noreply.github.com>
2022-12-06 08:58:24 +1000

    Add ruby plugin for asdf

--- asdf.zsh
+++ asdf.zsh
@@ -5,4 +5,5 @@ rm $HOME/.tool-versions
 ln -s $(pwd)/config/asdf/tool-versions $HOME/.tool-versions
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add ruby
 asdf plugin-add direnv
