7db7d6aaa5d0d901462477804459b3302fbfe05b
Arun Malik <3743409+malikarun@users.noreply.github.com>
2022-12-12 10:06:07 +1000

    Add Docker plugins setup

--- setup.zsh
+++ setup.zsh
@@ -16,6 +16,12 @@ source $HOME/.zshrc
 rm $HOME/.zprofile # remove existing zprofile
 ln $(pwd)/config/zsh/zprofile $HOME/.zprofile #link configured zprofile to home folder
 
+# Docker
+rm -rf ~/.docker/cli-plugins
+mkdir -p ~/.docker/cli-plugins
+ln -sfn $(which docker-compose) ~/.docker/cli-plugins/docker-compose
+([ -f ~/.docker/config.json ] && grep -q credStore ~/.docker/config.json) || echo '{"credsStore":"osxkeychain"}' > ~/.docker/config.json
+
 # Nvim
 ./config/nvim/packer.zsh # Install Packer
 
