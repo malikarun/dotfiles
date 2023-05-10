a237bf2a2d51ceda7ba9b975793334c0afb7c701
Arun Malik <3743409+malikarun@users.noreply.github.com>
2023-05-11 09:31:58 +1000

    Rename config dir to .config

--- asdf.zsh
+++ asdf.zsh
@@ -2,7 +2,7 @@ rm -rf $HOME/.asdf
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
 
 rm $HOME/.tool-versions
-ln -s $(pwd)/config/asdf/tool-versions $HOME/.tool-versions
+ln -s $(pwd)/.config/asdf/tool-versions $HOME/.tool-versions
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs
--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,6 @@
 # link the config directory to home folder
 rm -rf $HOME/.config
-ln -s $(pwd)/config $HOME/.config
+ln -s $(pwd)/.config $HOME/.config
 
 # Install Homebrew
 ./homebrew.zsh
@@ -9,12 +9,12 @@ ln -s $(pwd)/config $HOME/.config
 ./asdf.zsh
 
 # ZSH
-./config/zsh/install.zsh #install
+./.config/zsh/install.zsh #install
 rm $HOME/.zshrc # remove existing zshrc
-ln $(pwd)/config/zsh/zshrc $HOME/.zshrc #link configured zshrc to home folder
+ln $(pwd)/.config/zsh/zshrc $HOME/.zshrc #link configured zshrc to home folder
 source $HOME/.zshrc
 rm $HOME/.zprofile # remove existing zprofile
-ln $(pwd)/config/zsh/zprofile $HOME/.zprofile #link configured zprofile to home folder
+ln $(pwd)/.config/zsh/zprofile $HOME/.zprofile #link configured zprofile to home folder
 
 # Docker
 rm -rf ~/.docker/cli-plugins
@@ -23,24 +23,24 @@ ln -sfn $(which docker-compose) ~/.docker/cli-plugins/docker-compose
 ([ -f ~/.docker/config.json ] && grep -q credStore ~/.docker/config.json) || echo '{"credsStore":"osxkeychain"}' > ~/.docker/config.json
 
 # Nvim
-./config/nvim/packer.zsh # Install Packer
+./.config/nvim/packer.zsh # Install Packer
 
 # tmux
 rm $HOME/.tmux.conf # remove existing tmux conf
-ln $(pwd)/config/tmux/tmux.conf $HOME/.tmux.conf #link configured tmux file
+ln $(pwd)/.config/tmux/tmux.conf $HOME/.tmux.conf #link configured tmux file
 
 # git
 rm $HOME/.gitignore # remove existing gitignore
-ln $(pwd)/config/git/gitignore  $HOME/.gitignore # link configured gitignore file
+ln $(pwd)/.config/git/gitignore  $HOME/.gitignore # link configured gitignore file
 
 rm $HOME/.gitconfig # remove existing git conf
-ln $(pwd)/config/git/gitconfig  $HOME/.gitconfig # link configured git config file
+ln $(pwd)/.config/git/gitconfig  $HOME/.gitconfig # link configured git config file
 
 # ssh
-./config/ssh/setup.zsh
+./.config/ssh/setup.zsh
 
 # github
-./config/github/ssh-keys.zsh
+./.config/github/ssh-keys.zsh
 
 # node
 ./node.zsh
