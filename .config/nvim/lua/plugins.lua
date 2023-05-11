ca1420b788b8ab7205bc3ffb0961188e25e1e1e0
Arun Malik <3743409+malikarun@users.noreply.github.com>
2023-05-11 10:00:12 +1000

    Remove automatic packer installation

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,18 +1,3 @@
--- Automatically install packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-local packer = require('packer')
-
 return packer.startup(function(use)
   use { 'wbthomason/packer.nvim', opt = false }
   use 'tpope/vim-fugitive'
@@ -166,10 +151,4 @@ return packer.startup(function(use)
        require("bufferline").setup{}
      end
   }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
